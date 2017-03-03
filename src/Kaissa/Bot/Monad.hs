7031d9520f91ddd05c5d05c6494a86ae420b296b
Anton Gushcha <ncrashed@gmail.com>
2017-03-03 04:14:12 +0300

    Add execution of telegram API actions

--- src/Kaissa/Bot/Monad.hs
+++ src/Kaissa/Bot/Monad.hs
@@ -8,6 +8,7 @@ module Kaissa.Bot.Monad(
   , serverMToHandler
   -- * Helpers
   , getConfig
+  , runTelegram
   , logInfo
   , logWarn
   , logError
@@ -15,15 +16,24 @@ module Kaissa.Bot.Monad(
   , showt
   ) where
 
+import Control.Lens
 import Control.Monad.Base
+import Control.Monad.Except
 import Control.Monad.Logger
 import Control.Monad.Reader
 import Control.Monad.Trans.Control
+import Data.ByteString (ByteString)
+import Data.Monoid
 import Data.Text (Text, pack)
+import Data.Text.Encoding
 import Kaissa.Bot.Config
 import Network.HTTP.Client      (newManager, Manager)
 import Network.HTTP.Client.TLS  (tlsManagerSettings)
+import Servant.Common.Req
 import Servant.Server
+import Web.Telegram.API.Bot.API
+
+import qualified Data.ByteString.Lazy as BL
 
 -- | Environment of 'ServerM' monad
 data ServerEnv = ServerEnv {
@@ -44,7 +54,7 @@ newServerEnv cfg = do
 
 -- | Main monad of server, each handler of API operates in the monad.
 newtype ServerM a = ServerM { unServerM :: ReaderT ServerEnv (LoggingT Handler) a }
-  deriving (Functor, Applicative, Monad, MonadIO, MonadBase IO, MonadLogger)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadBase IO, MonadLogger, MonadError ServantErr)
 
 -- | Helper for 'MonadBaseControl' instance
 newtype StMServerM a = StMServerM { unStMServerM :: StM (ReaderT ServerEnv (LoggingT Handler)) a }
@@ -69,8 +79,29 @@ runServerM e m = runStdoutLoggingT $ runReaderT (unServerM m) e
 
 -- | Transformation from 'ServerM' monad to 'Handler'
 serverMToHandler :: ServerEnv -> ServerM :~> Handler
-serverMToHandler e = NT (runServerM e)
+serverMToHandler e = Nat (runServerM e)
 
 -- | Shortcut for displaying something as text
 showt :: Show a => a -> Text
 showt = pack . show
+
+-- | Shortcut for displaying something as bytestring
+showb :: Show a => a -> ByteString
+showb = encodeUtf8 . showt
+
+-- | Run telegram request in server monad
+runTelegram :: TelegramClient a -> ServerM (Either ServantError a)
+runTelegram m = do
+  cfg <- getConfig
+  mng <- getManager
+  liftIO $ runClient m (Token $ cfg ^. configToken) mng
+
+-- | Run telegram request in server monad and rethrow errors and log them
+runTelegram' :: TelegramClient a -> ServerM a
+runTelegram' m = do
+  res <- runTelegram m
+  case res of
+    Left err -> do
+      logInfoN $ "runTelegram: " <> showt err
+      throwError err500 { errBody = BL.fromStrict $ showb err }
+    Right a -> pure a
